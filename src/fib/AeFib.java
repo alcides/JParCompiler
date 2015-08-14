7df8120a217fe3737b2a9dc56f772688e1733ce0
Alcides Fonseca <me@alcidesfonseca.com>
2015-08-14 17:18:24 +0100

    Another commit

--- src/fib/AeFib.java
+++ src/fib/AeFib.java
@@ -1,5 +1,7 @@
 package fib;
 
+import java.util.Arrays;
+
 import aeminium.runtime.Body;
 import aeminium.runtime.Hints;
 import aeminium.runtime.Runtime;
@@ -10,42 +12,44 @@ import aeminium.runtime.futures.codegen.Sequential;
 
 @Sequential
 public class AeFib {
-
-	public static class FibBody implements Body {
-		public volatile long value;
-
-		public FibBody(long n) {
-			this.value = n;
-		}
-
-		public long seqFib(long n) {
-			if (n <= 2) return 1;
-			else return (seqFib(n - 1) + seqFib(n - 2));
-		}
-
+	
+	public abstract static class FBody<T> implements Body {
 		@Override
 		public void execute(Runtime rt, Task current) {
-			if (!rt.parallelize(current)) {
-				current.setResult((long) seqFib(value));
-			} else {
-				if (value <= 2) {
-					current.setResult((long) 1);
-					return;
-				}
-
-				FibBody b1 = new FibBody(value - 1);
-				Task t1 = rt.createNonBlockingTask(b1, Hints.RECURSION);
-				rt.schedule(t1, Runtime.NO_PARENT, Runtime.NO_DEPS);
-
-				FibBody b2 = new FibBody(value - 2);
-				Task t2 = rt.createNonBlockingTask(b2, Hints.RECURSION);
-				rt.schedule(t2, Runtime.NO_PARENT, Runtime.NO_DEPS);
-
-				value = (long) t1.getResult() + (long) t2.getResult();
-				current.setResult((long) value);
-			}
+			current.setResult(evaluate(current));
 		}
+		public abstract T evaluate(Task t);
 	}
+	
+	public static Task createTask(Body b, Task... ts) {
+		Task t = RuntimeManager.rt.createNonBlockingTask(b, Runtime.NO_HINTS);
+		RuntimeManager.rt.schedule(t, Runtime.NO_PARENT, Arrays.asList(ts));
+		return t;
+	}
+	
+	public static Task createTask(Body b) {
+		Task t = RuntimeManager.rt.createNonBlockingTask(b, Runtime.NO_HINTS);
+		RuntimeManager.rt.schedule(t, Runtime.NO_PARENT, Runtime.NO_DEPS);
+		return t;
+	}
+	
+	public static long seqFib(long n) {
+		if (n <= 2) return 1;
+		else return (seqFib(n - 1) + seqFib(n - 2));
+	}
+	
+	public static long parFib(long n) {
+		if (RuntimeManager.shouldSeq()) {
+			return seqFib(n);
+		}
+		if (n <= 2) return 1;
+		Task t1 = createTask((Runtime r, Task t) -> { t.setResult(parFib(n-1)); });
+		Task t2 = createTask((Runtime r, Task t) -> { t.setResult(parFib(n-2)); });
+		long v1 = (long) t1.getResult();
+		long v2 = (long) t2.getResult();
+		return v1 + v2;
+	}
+	
 
 	public static void main(String[] args) {
 
@@ -53,10 +57,7 @@ public class AeFib {
 
 		RuntimeManager.init();
 		long t = System.nanoTime();
-		FibBody body = new AeFib.FibBody(fib);
-		Task t1 = RuntimeManager.rt.createNonBlockingTask(body, Runtime.NO_HINTS);
-		RuntimeManager.rt.schedule(t1, Runtime.NO_PARENT, Runtime.NO_DEPS);
-		long v = (long) t1.getResult();
+		long v = parFib(fib);
 		java.lang.System.out.println(("% " + (((double)(((System.nanoTime()) - t))) / ((1000 * 1000) * 1000))));
 		System.out.println("R: " + v);
 		RuntimeManager.shutdown();
