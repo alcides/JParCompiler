80f9e1a91dbeb59c567fe40731ba4e973264eab7
Alcides Fonseca <me@alcidesfonseca.com>
2015-08-18 23:19:08 +0100

    Just one config

--- utils/all_configs.sh
+++ utils/all_configs.sh
@@ -2,7 +2,7 @@ source utils/library.sh
 
 #ATC=3 LEVEL=12 run_config atc3l12_nocache
 #LOADBASED=1 run_config loadbased_nocache
-#MAXLEVEL=12 run_config maxlevel12_nocache
+MAXLEVEL=12 run_config maxlevel12_nocache
 #MAXTASKS=3 run_config maxtasks3_nocache
 #MAXTASKSSS=3 SS=16 run_config maxtasks3ss16_nocache
 #MAXTASKINQ=3 run_config maxtaskinqueue3_nocache
@@ -10,23 +10,23 @@ source utils/library.sh
 #SURPLUS=3 run_config surplus3_nocache
 #SYSMON=70 MEM=70 run_config sysmon2-70-70_nocache
 
-BINARYSPLIT="true" ATC=3 LEVEL=12 run_config atc3l12_nocache_binary
-BINARYSPLIT="true" LOADBASED=1 run_config loadbased_nocache_binary
-BINARYSPLIT="true" MAXLEVEL=12 run_config maxlevel12_nocache_binary
-BINARYSPLIT="true" MAXTASKS=3 run_config maxtasks3_nocache_binary
-BINARYSPLIT="true" MAXTASKSSS=3 SS=16 run_config maxtasks3ss16_nocache_binary
-BINARYSPLIT="true" MAXTASKINQ=3 run_config maxtaskinqueue3_nocache_binary
-BINARYSPLIT="true" STACKSIZE=16 run_config stacksize16_nocache_binary
-BINARYSPLIT="true" SURPLUS=3 run_config surplus3_nocache_binary
-BINARYSPLIT="true" SYSMON=70 MEM=70 run_config sysmon2-70-70_nocache_binary
+#BINARYSPLIT="true" ATC=3 LEVEL=12 run_config atc3l12_nocache_binary
+#BINARYSPLIT="true" LOADBASED=1 run_config loadbased_nocache_binary
+#BINARYSPLIT="true" MAXLEVEL=12 run_config maxlevel12_nocache_binary
+#BINARYSPLIT="true" MAXTASKS=3 run_config maxtasks3_nocache_binary
+#BINARYSPLIT="true" MAXTASKSSS=3 SS=16 run_config maxtasks3ss16_nocache_binary
+#BINARYSPLIT="true" MAXTASKINQ=3 run_config maxtaskinqueue3_nocache_binary
+#BINARYSPLIT="true" STACKSIZE=16 run_config stacksize16_nocache_binary
+#BINARYSPLIT="true" SURPLUS=3 run_config surplus3_nocache_binary
+#BINARYSPLIT="true" SYSMON=70 MEM=70 run_config sysmon2-70-70_nocache_binary
 
 
-CACHE=50 ATC=3 LEVEL=12 run_config atc3l12_cache50
-CACHE=50 LOADBASED=1 run_config loadbased_cache50
-CACHE=50 MAXLEVEL=12 run_config maxlevel12_cache50
-CACHE=50 MAXTASKS=3 run_config maxtasks3_cache50
-CACHE=50 MAXTASKSSS=3 SS=16 run_config maxtasks3ss16_cache50
-CACHE=50 MAXTASKINQ=3 run_config maxtaskinqueue3_cache50
-CACHE=50 STACKSIZE=16 run_config stacksize16_cache50
-CACHE=50 SURPLUS=3 run_config surplus3_cache50
-CACHE=50 SYSMON=70 MEM=70 run_config sysmon2-70-70_cache50
+#CACHE=50 ATC=3 LEVEL=12 run_config atc3l12_cache50
+#CACHE=50 LOADBASED=1 run_config loadbased_cache50
+#CACHE=50 MAXLEVEL=12 run_config maxlevel12_cache50
+#CACHE=50 MAXTASKS=3 run_config maxtasks3_cache50
+#CACHE=50 MAXTASKSSS=3 SS=16 run_config maxtasks3ss16_cache50
+#CACHE=50 MAXTASKINQ=3 run_config maxtaskinqueue3_cache50
+#CACHE=50 STACKSIZE=16 run_config stacksize16_cache50
+#CACHE=50 SURPLUS=3 run_config surplus3_cache50
+#CACHE=50 SYSMON=70 MEM=70 run_config sysmon2-70-70_cache50
