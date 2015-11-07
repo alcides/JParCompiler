9175c1cf8779a03445e46a81db2b840999eae435
Alcides Fonseca <me@alcidesfonseca.com>
2015-11-07 23:28:16 +0000

    Script v2

--- utils/paper_granularity.sh
+++ utils/paper_granularity.sh
@@ -1,10 +1,10 @@
 source utils/library.sh
 
-ant jar
-CONFIGNAME=par_aggregated	test par_fft			run fft.FFT 16777216
+#ant jar
+#CONFIGNAME=par_aggregated	test par_fft			run fft.FFT 16777216
 
-PARALLELIZE=all ant jar
-CONFIGNAME=par_all 	test par_fft			run fft.FFT 16777216
+#PARALLELIZE=all ant jar
+#CONFIGNAME=par_all 	test par_fft			run fft.FFT 16777216
 
-MEMORYMODEL=1 ant jar
+MEMORYMODEL=1 PARALLELIZE=all ant jar
 CONFIGNAME=par_memory 	test par_fft			run fft.FFT 16777216
