1d06eec28aebbbd00659463c3957a0ce5e19906f
Alcides Fonseca <me@alcidesfonseca.com>
2015-11-08 00:03:17 +0000

    Nbody script

--- utils/paper_granularity.sh
+++ utils/paper_granularity.sh
@@ -1,10 +1,15 @@
 source utils/library.sh
 
-#ant jar
+ant jar
 #CONFIGNAME=par_aggregated	test par_fft			run fft.FFT 16777216
+CONFIGNAME=par_aggregated	test par_nbody			run nbody.NBody 10 25000
 
-#PARALLELIZE=all ant jar
+PARALLELIZE=all ant jar
 #CONFIGNAME=par_all 	test par_fft			run fft.FFT 16777216
+CONFIGNAME=par_all		test par_nbody			run nbody.NBody 10 25000
 
-MEMORYMODEL=1 PARALLELIZE=all ant jar
-CONFIGNAME=par_memory 	test par_fft			run fft.FFT 16777216
+#MEMORYMODEL=1 PARALLELIZE=all ant jar
+#CONFIGNAME=par_memory_all 	test par_fft			run fft.FFT 16777216
+
+#MEMORYMODEL=1 ant jar
+#CONFIGNAME=par_memory_aggregated 	test par_fft			run fft.FFT 16777216
