1252882246ccc0ba3696ba1ec4c237f134992794
Alcides Fonseca <me@alcidesfonseca.com>
2015-11-08 00:33:53 +0000

    Only parallel

--- utils/paper_granularity.sh
+++ utils/paper_granularity.sh
@@ -1,8 +1,8 @@
 source utils/library.sh
 
-ant jar
+#ant jar
 #CONFIGNAME=par_aggregated	test par_fft			run fft.FFT 16777216
-CONFIGNAME=par_aggregated	test par_nbody			run nbody.NBody 10 25000
+#CONFIGNAME=par_aggregated	test par_nbody			run nbody.NBody 10 25000
 
 PARALLELIZE=all ant jar
 #CONFIGNAME=par_all 	test par_fft			run fft.FFT 16777216
